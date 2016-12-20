d4eb0540ec07fb5b304f63db87f34de4251bd421
taplony1024 <it.sunday.develop@gmail.com>
2016-12-20 21:35:58 +0700

    First Upload

---
+++ cryptography.php
@@ -0,0 +1,61 @@
+<?php 
+/* 
+   PHP Cryptography by ITCS's Developer 
+   Release At 8:36 PM 12/20/2016
+
+   How To Use
+   include('cryptography.php');
+  
+   $encrypt = Encrypt("Some words", "SecretKey");
+   $decrypt = Decrypt("Encrypt words", "SecretKey");
+*/
+
+function GenHashTable($secretKey)
+{
+	$hastTable =array();
+	for ($i=0; $i <= 255 ; $i++) { 
+		array_push($hastTable,array('key'=>$i, 'code'=>  substr(md5($i.$secretKey),0,6) ));
+	}
+	return $hastTable;
+}
+
+function Decrypt($word, $secretKey)
+{
+	$_deWord = '';
+	if($word)
+	{
+		$arrayWord = str_split($word,6);
+		$hastTable = GenHashTable($secretKey);
+		foreach ($arrayWord as $key => $code) {
+
+			$found = false;
+			foreach ($hastTable as $key => $codeTable) {
+				if($code == $codeTable['code'])
+				{
+					$_deWord .= chr($codeTable['key']);
+					$found = true;
+				}
+			}
+
+			if(!$found)
+			{
+				$_deWord .= '#';
+			}
+		}
+	}
+	return $_deWord;
+}
+
+function Encrypt($word, $secretKey)
+{
+	$_enWord = '';
+	if($word)
+	{
+		$word = str_split($word);
+		foreach ($word as $key => $char) {
+			$_enWord .= substr(md5(ord($char) . $secretKey),0,6);
+		}
+	}
+	return $_enWord;
+}
+?>
---
+++ demo.php
@@ -0,0 +1,37 @@
+<?php 
+include('cryptography.php');
+
+?>
+
+
+<table>
+	<tr>
+		<td>
+			<form>
+				<label>Input Text To Encrypt</label><br>
+				<input type="text" name="encrypt">
+				<input type="text" name="secretKey" value="MySecretKey!@#$%^">
+				<button type="submit">Submit</button>
+			</form>
+			<label>Output Encrypt : </label><br>
+			<textarea cols="60" rows="20"><?php echo Encrypt(@$_GET['encrypt'], @$_GET['secretKey']); ?></textarea>
+		</td>
+
+		<td>
+			<form>
+				<label>Input Text To Decript</label><br>
+				<input type="text" name="decrypt">
+				<input type="text" name="secretKey"  value="MySecretKey!@#$%^">
+				<button type="submit">Submit</button>
+			</form>
+			<label>Output Decript : </label><br>
+			<textarea cols="60" rows="20"><?php echo Decrypt(@$_GET['decrypt'],@$_GET['secretKey']); ?></textarea>
+		</td>
+	</tr>
+</table>
+
+
+<br>
+<label>Demo By ITCS's Devloper</label>
+<label>PHP Cryptography by ITCS's Developer </label>
+<label>Release At 8:36 PM 12/20/2016</label>
